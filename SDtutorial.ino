0d21a06cfa88a798c2c4ef70eaf3df059de22a80
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2022-11-29 17:58:18 -0600

    Update SDtutorial.ino

--- SDtutorial.ino
+++ SDtutorial.ino
@@ -15,17 +15,20 @@ String b;                                                         // cadena de c
 
 void setup()
 {
-Serial.begin(9600);                                                // comunicación serial
-if (!SD.begin(4)) {Serial.println("!SD"); return;}    // inicia la tarjeta SD en puerto 4
-                                                           // Por defecto D4 es CS de SPI
+Serial.begin(9600);
+if (!SD.begin(4))                                     // inicia la tarjeta SD en puerto 4
+{Serial.println("!SD"); return;}
+
 SD.exists("Bez.txt");                                  // Comprueba que exista el archivo
-                                                           // Puede incluir sudirectorios
+
 Bez = SD.open("Bezier.txt");                                // Abre el archivo Bezier.txt
-while (Bez.available()) { Serial.write(Bez.read());}         // Lee el archivo Bezier.txt
-                                           // Sin el while sólo se escribe el primer byte
+while (Bez.available())                            // mientras el archivo esté disponible
+{ Serial.write(Bez.read());}         // Lee el archivo Bezier.txt y escríbelo en pantalla
+
 A = Bez.size(); Serial.println(A);                        // Evalúa el tamaño del archivo
 
-Bez.seek(0); a = Bez.read(); Serial.println(a);            // Se ubica en el n-ésimo byte
+Bez.seek(0);                                               // Se ubica en el n-ésimo byte
+a = Bez.read(); Serial.println(a);                                      // se lee el byte
 
 Bez.seek(0);
 while (Bez.available())
@@ -33,9 +36,9 @@ while (Bez.available())
   a = Bez.read();
   b = b + a;                                                      // concatena caracteres
   C = Bez.position();                                        // Evalúa la posición actual
-  if(a==10) { break; }
+  if(a==10) { break; }                                   // 10 en ascii es salto de línea
   }
-  Serial.print(b); Serial.println(b[3]);
+  Serial.print(b); Serial.println(b[3]);    // se imprime el tercer caracter de la cadena
 
 Bez.close();                                                         // Cierra el archivo
 
